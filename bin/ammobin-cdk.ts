a54b3425236046f278f9b9a4510a3583506e2c8d
nexus-uw <you@example.com>
2019-11-22 07:18:55 +0000

    created small stack to create s3 upload permssions
    - upload + generate is planned to run on azure pipelines (b/c its free + can run on a schedule)

--- bin/ammobin-cdk.ts
+++ bin/ammobin-cdk.ts
@@ -3,6 +3,7 @@ import cdk = require('@aws-cdk/core')
 import { AmmobinCdkStack } from '../lib/ammobin-cdk-stack'
 import { AmmobinGlobalCdkStack } from '../lib/ammobin-global-cdk-stack'
 import { GrafanaIamStack } from '../lib/grafana-iam-stack'
+import { s3UploadStack } from '../lib/s3-upload-stack'
 
 const app = new cdk.App()
 
@@ -21,6 +22,13 @@ new AmmobinCdkStack(app, 'AmmobinCdkStack', {
 }) //.addDependency(globalAmmo)
 
 new GrafanaIamStack(app, 'GrafanaIamStack', {
+  env: {
+    region: 'ca-central-1',
+  },
+})
+
+new s3UploadStack(app, 's3UploadStack', {
+  bucketArn: 'arn:aws:s3:::ammobin-aws-site',
   env: {
     region: 'ca-central-1',
   },
---
+++ lib/s3-upload-stack.ts
@@ -0,0 +1,28 @@
+import cdk = require('@aws-cdk/core')
+import iam = require('@aws-cdk/aws-iam')
+interface Props extends cdk.StackProps {
+  /**
+   * ARN of bucket to grant putObject
+   * if ommitted, grant to all buckets
+   */
+  bucketArn?: string
+}
+
+/**
+ * generate static assets on a schedule + upload to s3 site bucket
+ */
+export class s3UploadStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props: Props) {
+    super(scope, id, props)
+
+    // https://grafana.com/docs/features/datasources/cloudwatch/
+    const grafanaIAMUser = new iam.User(this, 's3Upload', {})
+    grafanaIAMUser.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        "s3:PutObject"
+      ],
+      resources: [props.bucketArn || '*']
+    }))
+  }
+}
