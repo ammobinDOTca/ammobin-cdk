12e3873263ce3a0be007c117d3f3d1d10ec66585
nexus-uw <nexus-uw@users.noreply.github.com>
2020-01-18 07:56:40 +0000

    constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,8 @@ import { RetentionDays } from "@aws-cdk/aws-logs"
 export const LOG_RETENTION = RetentionDays.ONE_MONTH
 
 export declare type Stage = 'prod' | 'beta'
+
+export declare type Region = 'CA'
+
+export const serviceName = 'ammobin'
+
