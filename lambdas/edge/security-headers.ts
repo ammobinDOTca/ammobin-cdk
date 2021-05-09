a2487df634b6c44f19559df75bb78e4347978f38
nexus-uw <nexus-uw@users.noreply.github.com>
2021-05-09 18:19:38 +0000

    added canada first cdn for sponsor img

--- lambdas/edge/security-headers.ts
+++ lambdas/edge/security-headers.ts
@@ -17,7 +17,7 @@ export function handler(event: CloudFrontResponseEvent, context: Context, cb: Ca
     'content-security-policy': [{
       key: 'Content-Security-Policy',
       // todo: serve workbox myself
-      value: "default-src 'self';script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://storage.googleapis.com; connect-src  'self';  style-src 'self' 'unsafe-inline';img-src 'self';" //  report-uri https://ammobin.ca/api/content-security-report-uri" // dont bother reporting this, noise is not worth the bill
+      value: "default-src 'self';script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://storage.googleapis.com; connect-src  'self';  style-src 'self' 'unsafe-inline';img-src 'self' https://store-udt1amkaxd.mybigcommerce.com;" //  report-uri https://ammobin.ca/api/content-security-report-uri" // dont bother reporting this, noise is not worth the bill
     }],
     'referrer-policy': [{ key: 'Referrer-policy', value: 'origin' }],
     ...response.headers
