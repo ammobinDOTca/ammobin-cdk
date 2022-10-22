b05b6d4ae0e3199643b17e5875f4f77b6c5b5a29
nexus-uw <nexus-uw@users.noreply.github.com>
2022-10-22 22:41:21 +0000

    test logging for signer

--- lambdas/edge-signer/index.ts
+++ lambdas/edge-signer/index.ts
@@ -1,1 +1,8 @@
-export { handler } from 'edge-lambda-url-authorizer'
+// export { handler } from 'edge-lambda-url-authorizer'
+import { handler as fn } from 'edge-lambda-url-authorizer'
+
+
+export function handler(event,context,cb){
+    console.log(JSON.stringify(event),context)
+    return fn(event,context,cb)
+}
