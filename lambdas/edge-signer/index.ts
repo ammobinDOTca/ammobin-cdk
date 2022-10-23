83c4a239a7349332faff71697213a5498d4f5ec2
nexus-uw <nexus-uw@users.noreply.github.com>
2022-10-23 22:20:01 +0000

    update signer to 0.1.0

--- lambdas/edge-signer/index.ts
+++ lambdas/edge-signer/index.ts
@@ -1,8 +1,1 @@
-// export { handler } from 'edge-lambda-url-authorizer'
-import { handler as fn } from 'edge-lambda-url-authorizer'
-
-
-export function handler(event,context,cb){
-    console.log(JSON.stringify(event),context)
-    return fn(event,context,cb)
-}
+export { handler } from 'edge-lambda-url-authorizer'
